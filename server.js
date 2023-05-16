06bd7fbf9d3c832c04d7432f7a67da84648a9ff2
JoRuPeRu <johan.rutberg@iths.se>
2023-05-16 10:50:51 +0200

    Server setup

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.Promise = global.Promise;
+mongoose
+    .connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xwkradj.mongodb.net/test`, {
+        useNewUrlParser: true
+    })
+    .then(() => {
+        console.log('Successfully connected to the database');
+    })
+    .catch((err) => {
+        console.log('Could not connect to the database. Error...', err);
+        process.exit();
+    });
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Server is running :D' });
+});
+
+let PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
